0f80f940a7bb5d8eacb15ae17dd7b0e3b7bdafb0
Tristan Boudra <boudratristan@gmail.com>
2022-05-20 18:24:31 +0200

    fonctionnalité like

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,12 +11,24 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snap!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit(): void {
     this.title = 'Lune';
     this.description = 'La Lune est le satellite naturelle de la Terre';
     this.createdDate = new Date();
-    this.snap = 6;
+    this.snap = 0;
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.buttonText = 'Oh Snap!';
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.snap++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.snap--;
+      this.buttonText = 'Oh Snap!';
+    }
   }
 }
