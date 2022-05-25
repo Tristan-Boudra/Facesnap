763125e647ff1d2df1b58e7cbc5858808347cab6
Tristan Boudra <boudratristan@gmail.com>
2022-05-25 10:03:46 +0200

    ajout de classe dynamique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,35 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = {
-      title: 'Lune', 
-      description: 'La Lune est le satellite naturelle de la Terre',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snap: 0,
-      location: 'Paris'
-    };
-    this.myOtherSnap = {
-      title: 'Lune', 
-      description: 'La Lune est le satellite naturelle de la Terre',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snap: 0,
-      location: 'Lyon'
-    };
-    this.myLastSnap = {
-      title: 'Lune', 
-      description: 'La Lune est le satellite naturelle de la Terre',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snap: 0,
-    };
+    this.faceSnaps = [
+      {
+        title: 'Lune', 
+        description: 'La Lune est le satellite naturelle de la Terre',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snap: 1,
+        location: 'Paris'
+      },
+      {
+        title: 'Lune', 
+        description: 'La Lune est le satellite naturelle de la Terre',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snap: 9,
+        location: 'Lyon'
+      },
+      {
+        title: 'Lune', 
+        description: 'La Lune est le satellite naturelle de la Terre',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snap: 10,
+      },
+    ]
   }
 }
 
