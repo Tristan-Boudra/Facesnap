805ad3baee51c542c2a1bbff00c4248c59ab3462
Tristan Boudra <boudratristan@gmail.com>
2022-05-26 14:30:18 +0200

    utilisation de service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -8,34 +7,7 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit{
 
-  faceSnaps!: FaceSnap[];
-
   ngOnInit(): void {
-    this.faceSnaps = [
-      {
-        title: 'Lune', 
-        description: 'La Lune est le satellite naturelle de la Terre',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snap: 1,
-        location: 'Paris'
-      },
-      {
-        title: 'Lune', 
-        description: 'La Lune est le satellite naturelle de la Terre',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snap: 9,
-        location: 'Lyon'
-      },
-      {
-        title: 'Lune', 
-        description: 'La Lune est le satellite naturelle de la Terre',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snap: 10,
-      },
-    ]
   }
 }
 
